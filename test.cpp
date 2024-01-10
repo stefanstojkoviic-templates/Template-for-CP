49d42e26070bd63fa0e81c4e6863089d5b66191c
Stefan Stojkovic <sstefan.kv748@gmail.com>
2024-01-10 13:30:08 +0100

    First commit

---
+++ test.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#define FAST ios::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define F frist
+#define S second
+#define PB push_back
+#define MP make_pair
+#define REP(i,a,b) for(int i=a;i<=b;i++)
+typedef long long ll;
+typedef vector<ll> vl;
+typedef pair<ll,ll> pl;
+
+
+using namespace std;
+
+void solve(){
+
+}
+
+
+int main(){
+	FAST;
+	ll t;cin>>t;
+	while(t--)solve();
+
+	return 0;
+}
+
